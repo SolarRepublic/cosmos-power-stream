c9bb2b011e7ed268b16b5c40518806bfdf6ff3ab
Blake Regalia <blake.regalia@gmail.com>
2024-07-05 03:03:37 -0700

    dev: inequality filter funcs

--- database/postgres/init.sql
+++ database/postgres/init.sql
@@ -42,3 +42,42 @@ CREATE INDEX idx_event_value_bytes ON event_values USING HASH(value_bytes);
 CREATE INDEX idx_event_tx_id ON events(tx_id);
 CREATE INDEX idx_event_path_id ON events(path_id);
 CREATE INDEX idx_event_value_id ON events(value_id);
+
+-- parameterizes the creation of inequality filters for events
+CREATE OR REPLACE FUNCTION create_filter_func_inequality(
+	suffix TEXT,
+	operator TEXT
+) RETURNS VOID AS $body$
+BEGIN
+	EXECUTE format($func$
+		CREATE OR REPLACE FUNCTION %I(
+			_path_text TEXT,
+			_value_bigint DECIMAL,
+			_value_unit VARCHAR(128) DEFAULT NULL
+		) RETURNS TABLE (
+			tx_id BIGINT,
+			value_text TEXT,
+			value_bigint DECIMAL
+		) AS $query$
+		BEGIN
+			RETURN QUERY
+			SELECT e.tx_id, v.value_text, v.value_bigint
+			FROM events e
+			INNER JOIN event_paths p ON e.path_id = p.id
+			INNER JOIN event_values v ON e.value_id = v.id
+			WHERE
+				p.path_text = _path_text
+				AND v.value_bigint %s _value_bigint
+				AND (_value_unit IS NULL OR v.value_unit = _value_unit);
+		END;
+		$query$ LANGUAGE plpgsql;
+	$func$, suffix, operator);
+END;
+$body$ LANGUAGE plpgsql;
+
+-- generate an event filter function for each inequality type
+SELECT create_filter_func_inequality('filter_event_lt', '<');
+SELECT create_filter_func_inequality('filter_event_gt', '>');
+SELECT create_filter_func_inequality('filter_event_lte', '<=');
+SELECT create_filter_func_inequality('filter_event_gte', '>=');
+
